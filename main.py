301e2dc9fafe93818b582315eedfb62038010dd0
gilesbillie01@gmail.com <gilesbillie01@gmail.com>
2023-07-26 14:00:01 +0200

    Added file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import gpt4all
 
 app = Flask(__name__)
 
-_gpt4all = gpt4all.GPT4All("/home/user/Documents/dev/OSINT Tool/models/wizardlm-13b-v1.1-superhot-8k.ggmlv3.q4_0.bin")
+_gpt4all = gpt4all.GPT4All("../models/wizardlm-13b-v1.1-superhot-8k.ggmlv3.q4_0.bin")
 session = _gpt4all.chat_session()
 
 
